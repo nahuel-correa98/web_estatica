af45b448b68cae6b20b0745842e9116bfa15fa66
nahuel correa <nahuel16.tigre@gmail.com>
2023-05-22 19:40:33 -0300

    agregado de image, web1 y web2

---
+++ README.md
@@ -0,0 +1,4 @@
+En la web1 tenemos una pagina de login de email. Es simple con un boton de subbit.(hace falta aplicar responsive y local o session storage)
+
+
+En la web2 tenemos una web de login de usuario que guarda el usuario y password. Tiene un audio mp3 de la cancion Bones de imagine dragons.(hace falta aplicar responsive y local o session storage)
---
+++ web2/main2.js
@@ -0,0 +1,16 @@
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const button = document.getElementById('button')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const data = {
+        username: username.value,
+        password: password.value
+    }
+    console.log(data)
+})
+
+
+
+
